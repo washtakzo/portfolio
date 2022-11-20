804290d86b8895255647ed067f1239a3e8603916
washtakzo <washtakzo@gmail.com>
2022-11-20 19:54:09 +0100

    fix footer style && all links

--- src/components/CTAButton.tsx
+++ src/components/CTAButton.tsx
@@ -2,13 +2,19 @@ import React from "react";
 
 type Props = {
   className?: string | null;
+  link?: string;
   children: any;
 };
 
-const CTAButton = ({ className, children }: Props) => {
+const CTAButton = ({ className, link, children }: Props) => {
   return (
     <button
       className={`bg-secondary text-white py-2 px-6 rounded-full text-lg ${className}`}
+      onClick={() => {
+        if (link) {
+          window.open(link, "_blank")?.focus();
+        }
+      }}
     >
       {children}
     </button>
