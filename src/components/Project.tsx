5eace024964d065ffb4271b35cdceeb133220b1f
washtakzo <washtakzo@gmail.com>
2022-11-21 10:10:44 +0100

    project fix font

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -19,13 +19,19 @@ const Project = ({
   order,
 }: Props) => {
   const orderStyle = "lg:order-[" + order + "]";
+  const projectClickHandler = () => {
+    if (link) window.open(link, "_blank")?.focus();
+  };
   return (
-    <div className="bg-red-100s my-8 px-4 lg:px-12 flex flex-col items-center justify-evenly lg:flex-row py-4 lg:py-28 cursor-pointer hover:scale-105 hover:shadow-xl transition duration-300 ease-out">
+    <div
+      className="bg-red-100s my-8 px-4 lg:px-12 flex flex-col items-center justify-evenly lg:flex-row py-4 lg:py-28 cursor-pointer hover:scale-105 hover:shadow-xl transition duration-300 ease-out"
+      onClick={projectClickHandler}
+    >
       <div className="bg-green-100s">
         <h4 className="text-center font-serif text-primary text-[22px] sm:text-[32px] lg:text-left">
           {title}
         </h4>
-        <p className="bg-orange-200s font-sans text-[16px] w-[70%] mx-auto lg:mx-0 leading-7 text-primary text-center py-4 sm:text-[18px] sm:py-8 lg:text-left lg:px-0 lg:w-[280px]">
+        <p className="bg-orange-200s font-sans text-[14px] sm:text-[16px] w-[70%] mx-auto lg:mx-0 leading-7 text-primary text-center py-4 sm:py-8 lg:text-left lg:px-0 lg:w-[280px]">
           {description}
         </p>
         <div className="project__grid mx-auto w-[220px] gap-1 grid-cols-2 sm:grid-cols-3 sm:w-[380px] lg:grid-cols-2 lg:w-[260px] lg:gap-3 lg:ml-0">
