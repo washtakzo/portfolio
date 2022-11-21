1228cf362d20bfbb73ffb5f3ed67b899ad7dbf35
washtakzo <washtakzo@gmail.com>
2022-11-21 11:05:39 +0100

    ?

--- src/App.tsx
+++ src/App.tsx
@@ -10,22 +10,22 @@ function App() {
   return (
     <>
       <Header />
-      <main className="lg:min-w-[1000px] mx-auto lg:w-[80%] max-w-[1600px] px-6 md:px-10">
-        <section className="lg:h-[90vh] lg:flex lg:flex-col justify-center">
-          <div className="flex flex-col my-16 gap-8 justify-between lg:flex-row lg:m-0">
+      <main className="mx-auto max-w-[1600px] px-6 md:px-10 lg:w-[80%] lg:min-w-[1000px]">
+        <section className="justify-center lg:flex lg:h-[90vh] lg:flex-col">
+          <div className="my-16 flex flex-col justify-between gap-8 lg:m-0 lg:flex-row">
             <img
-              className="w-48 rounded-lg mx-auto sm:w-[260px] lg:order-1 lg:w-[34%] lg:max-w-[380px] lg:mx-0"
+              className="mx-auto w-48 rounded-lg sm:w-[260px] lg:order-1 lg:mx-0 lg:w-[34%] lg:max-w-[380px]"
               src="https://uploads-ssl.webflow.com/62f3ee8686515cd11e272485/634d774671eca125c2837135_portfolio-sept-22-v3-p-800.jpg"
               alt="photo de profil"
             />
-            <div className="flex flex-col justify-between h-[242px]s sm:h-[280px] md:h-[340px]  lg:h-auto lg:justify-evenly lg:max-w-[740px]">
-              <h2 className="font-serif leading-[3rem] sm:leading-[3.8rem] md:leading-[4.2rem] text-3xl max-w-[445px] mx-auto sm:text-[42px] sm:max-w-none md:text-[52px]">
+            <div className="h-[242px]s flex flex-col justify-between sm:h-[280px] md:h-[340px]  lg:h-auto lg:max-w-[740px] lg:justify-evenly">
+              <h2 className="mx-auto max-w-[445px] font-serif text-3xl leading-[3rem] sm:max-w-none sm:text-[42px] sm:leading-[3.8rem] md:text-[52px] md:leading-[4.2rem]">
                 Des applications React orientées
                 <span className="text-secondary"> santé et bien-être, </span>
                 soignées et maintenables
               </h2>
               <CTAButton
-                className="block mx-auto mt-8 py-3 px-6 text-xl sm:m-0 sm:self-start"
+                className="mx-auto mt-8 block py-3 px-6 text-xl sm:m-0 sm:self-start"
                 link="https://calendly.com/zoher-ouazzar/30min"
               >
                 Contactez-moi
@@ -45,7 +45,7 @@ function App() {
         </section>
         <section className="py-20">
           <h3
-            className="font-serif text-[32px] text-center sm:text-[48px]"
+            className="text-center font-serif text-[32px] sm:text-[48px]"
             id="projects"
           >
             Projets
@@ -65,30 +65,29 @@ function App() {
           )}
         </section>
         <section className="py-20">
-          <h3 className="font-serif text-[32px] text-center sm:text-[48px] py-8x">
+          <h3 className="py-8x text-center font-serif text-[32px] sm:text-[48px]">
             Technologies
           </h3>
-          <div className="grid grid-cols-2 mx-auto sm:grid-cols-4 max-w-[940px] lg:gap-6 lg:my-[144px]">
+          <div className="mx-auto grid max-w-[940px] grid-cols-2 sm:grid-cols-4 lg:my-[144px] lg:gap-6">
             {technologies.map(({ image, title }) => (
               <Technologie key={title} image={image} title={title} />
             ))}
           </div>
         </section>
       </main>
-      <footer className="flex flex-col bg-primary text-white my-8 w-[94%] py-8 px-8 rounded-xl sm:py-20 lg:py-[60px] lg:px-[100px] max-w-[1200px] mx-auto">
-        <h5 className="text-[22px] sm:text-[28px] font-semibold text-center mb-8 md:text-[32px] lg:text-[42px]">
+      <footer className="my-8 mx-auto flex w-[94%] max-w-[1200px] cursor-pointer flex-col rounded-xl bg-primary py-8 px-8 text-white transition duration-300 ease-out hover:scale-105 sm:py-20 lg:py-[60px] lg:px-[100px]">
+        <h5 className="mb-8 text-center text-[22px] font-semibold sm:text-[28px] md:text-[32px] lg:text-[42px]">
           Parlons de vos projets et voyons ensemble comment je peux vous aider à
           les réaliser
         </h5>
         <CTAButton
-          className="mx-auto text-[15px] px-8 md:text-[16px]"
+          className="mx-auto px-8 text-[15px] transition duration-300 ease-out hover:scale-105 md:text-[16px]"
           link="https://calendly.com/zoher-ouazzar/30min"
         >
           Contactez-moi
         </CTAButton>
       </footer>
     </>
-    //Lets talk
   );
 }
 
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -18,34 +18,36 @@ const Project = ({
   link,
   order,
 }: Props) => {
-  const orderStyle = "lg:order-[" + order + "]";
   const projectClickHandler = () => {
     if (link) window.open(link, "_blank")?.focus();
   };
+  const orderStyle = "lg:order-[" + order + "]";
+  console.log(orderStyle);
+
   return (
     <div
-      className="bg-red-100s my-8 px-4 lg:px-12 flex flex-col items-center justify-evenly lg:flex-row py-4 lg:py-28 cursor-pointer hover:scale-105 hover:shadow-xl transition duration-300 ease-out"
+      className="my-8 flex cursor-pointer flex-col items-center justify-evenly px-4 py-4 transition duration-300 ease-out hover:scale-105 lg:mx-auto lg:w-[90%] lg:flex-row lg:px-12 lg:py-28"
       onClick={projectClickHandler}
     >
-      <div className="bg-green-100s">
-        <h4 className="text-center font-serif text-primary text-[22px] sm:text-[32px] lg:text-left">
+      <div>
+        <h4 className="text-center font-serif text-[22px] text-primary sm:text-[32px] lg:text-left">
           {title}
         </h4>
-        <p className="bg-orange-200s font-sans text-[14px] sm:text-[16px] w-[70%] mx-auto lg:mx-0 leading-7 text-primary text-center py-4 sm:py-8 lg:text-left lg:px-0 lg:w-[280px]">
+        <p className="mx-auto w-[70%] py-4 text-center font-sans text-[14px] leading-7 text-primary sm:py-8 sm:text-[16px] lg:mx-0 lg:w-[280px] lg:px-0 lg:text-left">
           {description}
         </p>
-        <div className="project__grid mx-auto w-[220px] gap-1 grid-cols-2 sm:grid-cols-3 sm:w-[380px] lg:grid-cols-2 lg:w-[260px] lg:gap-3 lg:ml-0">
+        <div className="project__grid mx-auto w-[220px] grid-cols-2 gap-1 sm:w-[380px] sm:grid-cols-3 lg:ml-0 lg:w-[260px] lg:grid-cols-2 lg:gap-3">
           {technologies.map((techno) => (
             <p
               key={techno}
-              className="mx-auto w-[100%] text-center py-3 px-4 rounded-full bg-gray-100 text-xs sm:text-sm"
+              className="mx-auto w-[100%] rounded-full bg-gray-100 py-3 px-4 text-center text-xs sm:text-sm"
             >
               {techno}
             </p>
           ))}
         </div>
       </div>
-      <div className={`${orderStyle}  mt-4 lg:px-8`}>
+      <div className={`${orderStyle} mt-4 lg:px-8`}>
         <img src={image} alt={title} className="" />
       </div>
     </div>
