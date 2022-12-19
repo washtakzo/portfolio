29aa5c606c1cb63635e3137aa231f1bbf6c23b72
washtakzo <washtakzo@gmail.com>
2022-12-19 14:45:51 +0100

    add noces de coton

--- src/assets/content.ts
+++ src/assets/content.ts
@@ -34,6 +34,30 @@ export const testimonials: Testimonial[] = [
 
 export const projects: Project[] = [
   {
+    title: "Noces de coton",
+    description: `Site e-commerce de faire-part et objets événementiel pour mariage, baby shower, naissance, etc...`,
+    technologies: [
+      "Mongo DB",
+      "Express",
+      "React Js",
+      "Node Js",
+      "Mongoose",
+      "Next Js",
+      "Tailwind",
+      "Stripe",
+    ],
+    image: "https://drive.google.com/uc?id=1oNUyt1xJlTjK8joL05aQIqvrr5lGPi_R",
+    link: "https://noces-de-coton.com",
+  },
+  {
+    title: "Netflix Clone",
+    description: `Clone de l'application web Netflix`,
+    technologies: ["React js", "Redux", "Firebase", "Router", "Stripe"],
+    image:
+      "https://uploads-ssl.webflow.com/62f3ee8686515cd11e272485/6357c5f0735ff409e2f6d154_1463%20v2-p-1600.png",
+    link: "https://netflix-clone-ee57f.web.app/",
+  },
+  {
     title: "Ikimass App",
     description: `Application Android original permettant de faire des séances personnalisées en notant ses performances rapidement et en gardant un oeil sur sa progression.`,
     technologies: [
@@ -49,6 +73,21 @@ export const projects: Project[] = [
     image:
       "https://uploads-ssl.webflow.com/62f3ee8686515cd11e272485/633f2d888a14aababe67bf1b_2211__mobile_only-p-1600.png",
     link: "https://play.google.com/store/apps/details?id=com.khuwarizmi.ikimasse",
+  },
+  {
+    title: "Hulu-Anime",
+    description: `Site de découverte d'animes`,
+    technologies: [
+      "React js",
+      "API GraphQL",
+      "Redux",
+      "Typescript",
+      "Nextjs",
+      "Tailwind",
+    ],
+    image:
+      "https://uploads-ssl.webflow.com/62f3ee8686515cd11e272485/6371e7c460a89c1180c3b841_hulu_anime_mockup3-p-2600.jpg",
+    link: "https://hulu-anime.vercel.app/",
   },
   {
     title: "Train&Drink",
@@ -67,35 +106,12 @@ export const projects: Project[] = [
     link: "https://train-and-drink.vercel.app/",
   },
   {
-    title: "Netflix Clone",
-    description: `Clone de l'application web Netflix`,
-    technologies: ["React js", "Redux", "Firebase", "Router", "Stripe"],
-    image:
-      "https://uploads-ssl.webflow.com/62f3ee8686515cd11e272485/6357c5f0735ff409e2f6d154_1463%20v2-p-1600.png",
-    link: "https://netflix-clone-ee57f.web.app/",
-  },
-  {
     title: "Your Sweets",
     description: `Site de vente de desserts réalisé en équipe dans le carde du bootcamp React Mastery.`,
     technologies: ["React js", "Context API", "Material UI"],
     image:
       "https://uploads-ssl.webflow.com/62f3ee8686515cd11e272485/635ffb0b733c63d8b979a976_v2-p-1600.jpg",
     link: "",
-  },
-  {
-    title: "Hulu-Anime",
-    description: `Site de découverte d'animes`,
-    technologies: [
-      "React js",
-      "API GraphQL",
-      "Redux",
-      "Typescript",
-      "Nextjs",
-      "Tailwind",
-    ],
-    image:
-      "https://uploads-ssl.webflow.com/62f3ee8686515cd11e272485/6371e7c460a89c1180c3b841_hulu_anime_mockup3-p-2600.jpg",
-    link: "https://hulu-anime.vercel.app/",
   },
 ];
 
